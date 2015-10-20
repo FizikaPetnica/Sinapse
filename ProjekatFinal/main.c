64347ae6ee960caa6798540f514ac89e05e35d26
Isidora Rapajic <isidora.rapajic97@gmail.com>
2015-10-20 17:54:36 +0200

    Final - ubaceni enzimi

--- ProjekatFinal/main.c
+++ ProjekatFinal/main.c
@@ -3,36 +3,39 @@
 #include <math.h>
 #include <time.h>
 
+int broj_receptora = 1000;
 double time_step = 0.01; //mikrosekunde
-int broj_iteracija = 200;
+int broj_iteracija = 500;
 double okolina = 0.02;
 int pocetni_broj_cestica = 3000;
 int broj_vezanih_cestica = 0;
+int broj_cestica = 3000;
 int broj_zauzetih_receptora = 0;
+int broj_razlozenih_cestica = 0;
 double visina = 0.02; //: mikromtri
 double duzina = 0.15; //:mikrometri
 double sirina = 0.02; //mikrometri
-int broj_receptora = 1000;
 int broj_enzima = 200;
+double kolicina[200];
 
 // definisem enzim
 struct enzim_st
 {
-	double enx;
-	double eny;
-	double enz;
+	double x;
+	double y;
+	double z;
 	double pu;
-} enzim;
+} enzim, enzimi[1000];
 
 // struktura receptor
 struct receptor_st
 {
-	double rx;
-	double ry;
-	double rz;
+	double x;
+	double y;
+	double z;
 	double pb;
 	int zauzeto;
-} receptor;
+} receptor, receptori[1000]; // iz nekog razloga ovde se mora staviti konkretan broj, u suprotnom javi gresku
 
 //struktura ACh
 typedef struct ACh_st
@@ -73,6 +76,7 @@ double randn(double mu, double sigma)
 
 	return (mu + sigma * (double)X1);
 }
+
 // procedura za ucitavanje u fajl
 void file(double x[], double y[])
 {
@@ -88,10 +92,9 @@ void file(double x[], double y[])
 
 }
 
-/*void rasporedjivanje_receptora(duzina, sirina, broj_receptora)
+void rasporedjivanje_receptora(duzina, sirina, broj_receptora)
 {
     int p;
-    receptori receptor1;
     double step_x, step_y;
 
     int odnos_duzina = (int)floor(broj_receptora/duzina);
@@ -102,13 +105,27 @@ void file(double x[], double y[])
 
     for (p = 0; p < broj_receptora; p++)
     {
-        receptor1.rx = -sirina / 2 + (p % odnos_sirina) * step_x;
-		receptor1.ry = -duzina / 2 + (p % odnos_duzina) * step_y;
-		receptor1.rz = -visina / 2;
+        receptori[p].x = -sirina / 2 + (p % odnos_sirina) * step_x;
+		receptori[p].y = -duzina / 2 + (p % odnos_duzina) * step_y;
+		receptori[p].z = -visina / 2;
+		receptori[p].zauzeto = 0;
+		receptori[p].pb = 1.5774265663 * sqrt(time_step);
     }
 
 }
-*/
+
+void rasporedjivanje_enzima(duzina, sirina, visina, broj_enzima)
+{
+    int g;
+    for (g = 0; g < broj_enzima; g++)
+    {
+                enzimi[g].x = ((double)rand() / RAND_MAX) * sirina/2;
+                enzimi[g].y = ((double)rand() / RAND_MAX) * duzina/2;
+                enzimi[g].z = ((double)rand() / RAND_MAX) * visina/2;
+                enzimi[g].pu = 1.5774265663 * sqrt(time_step);
+    }
+}
+
 
 ACh kretanje(ACh cestica)
 {
@@ -142,13 +159,48 @@ ACh kretanje(ACh cestica)
 
 }
 
+void vezivanje(ACh cestica)
+{
+    int p;
+	double r = (double)rand() / RAND_MAX;
+	double pb = 1.5774265663 * sqrt(time_step);
+
+		if ((abs(cestica.cy - receptor.y) <= okolina) && (r <= pb) && (abs(cestica.cx - receptor.x) <= okolina) && (cestica.cz >= 0) && (cestica.cz < visina))
+		{
+			if (receptor.zauzeto != 2)
+			{
+				receptor.zauzeto += 1;
+				broj_vezanih_cestica += 1;
+				broj_cestica = broj_cestica - 1;
+
+			}
+			else
+			{
+			    receptor.zauzeto = 0;
+				kretanje(cestica);
+			}
+        }
+
+}
+
+void razlaganje (ACh cestica)
+{
+    double r = (double)rand() / RAND_MAX;
+    enzim.pu = 1.5774265663 * sqrt(time_step);
+    if ((abs(cestica.cz - enzim.z) <= okolina) && (abs(cestica.cx - enzim.x) <= okolina) && (cestica.cy <= enzim.y) && (r <= enzim.pu))
+            {
+
+                       broj_cestica = broj_cestica - 1;
+                       broj_razlozenih_cestica = broj_razlozenih_cestica + 1;
+            }
+
+}
 int main()
 {
     srand((unsigned)time(NULL));
-    double kolicina[broj_iteracija];
 	double vreme[broj_iteracija];
 
-	int s;
+	int s, i;
     ACh cestica;
     ACh pom;
 	cestica.cx = 0;
@@ -162,14 +214,15 @@ int main()
 	{
     vreme[s] = s*time_step;
     pom = kretanje(cestica);
-    //vezivanje(cestica);
-    kolicina[s] = pom.cy;
-    FILE *isidora;
-	isidora = fopen("nesto.txt", "a");
-	fprintf(isidora, "%f %f %f \n", pom.cx, pom.cy, pom.cz);
-	fclose(isidora);
+    vezivanje(cestica);
+    razlaganje(cestica);
+    //FILE *isidora;
+	//isidora = fopen("nesto.txt", "a");
+	//fprintf(isidora, "%f %f %f \n", pom.cx, pom.cy, pom.cz);
+	//fclose(isidora);
+	kolicina[s] = broj_cestica;
 	}
-
+    printf("%d", broj_razlozenih_cestica);
 	file(kolicina, vreme);
 
     return 0;
